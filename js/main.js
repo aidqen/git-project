2eed60bf0145bc6163b9c4453d44e063e11a5b6d
aidqen <idanmarkin8@gmail.com>
2024-01-31 21:57:29 +0200

    Add transition to css

--- js/main.js
+++ js/main.js
@@ -8,4 +8,10 @@ function onBallClick() {
     ball.style.width = currValue + 50 + 'px' 
     ball.style.height = currValue + 50 + 'px' 
     ball.innerText = currValue + 50
+
+    if (currValue >= 400) {
+    ball.style.width = '100px' 
+    ball.style.height = '100px' 
+    ball.innerText = 100
+    }
 }
